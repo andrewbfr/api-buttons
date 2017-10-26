4c8c528bf40e84af8ad7ba03ef4edbd72f471213
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-10-26 15:29:40 -0600

    bueller, bueller, giphy, giphy

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -9,34 +9,43 @@
       // api key, 
       // user-input,
       // limit 10
-    // create new divs to display each returned gif individually
+    // function to create new divs to display each returned gif individually and display the rating
+    // function to call the giphy api on a click
     // make sure to call the response.name correctly from the giphy JSON
 
 
-
-
+console.log("one");
 //variables
 var apiKey = "9BlNYmwgDWvberRjgJV9QfSnyt8O6fja"
-var userInput = $("#zoo-input").val();
+var userInput = $("#zoo-input").val().trim();
 var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=9BlNYmwgDWvberRjgJV9QfSnyt8O6fja&limit=10";
 
 //functions
+console.log(userInput);
 
+console.log("two");
+function createDisplay(){
 
+};
+
+function handleGifs(){
+    $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response) {
+          console.log(response);
+
+        });
+};
 
 //main processes
-
-$(document).ready({
-
+$("#add-toast").click(function(){
+  handleGifs();
+  console.log(userInput);
+  console.log("click-button-works");
 });
 
+console.log("function inside of your function");
 
-$.ajax({
-      url: queryURL,
-      method: "GET"
-    }).done(function(response) {
-      console.log(response);
 
-      console.log(response);
-    });
 
