a69940517819001955a17689d88e57f01da720f2
Andrew Ferrer <AndBFer@Andrews-MBP.domain>
2017-10-27 19:51:37 -0600

    displaying new buttons, adding class, clearing input, appending to view div

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -12,38 +12,109 @@
     // function to create new divs to display each returned gif individually and display the rating
     // function to call the giphy api on a click
     // make sure to call the response.name correctly from the giphy JSON
+    // ajax
 
 
 console.log("one");
 //variables
 var apiKey = "9BlNYmwgDWvberRjgJV9QfSnyt8O6fja"
-var userInput = $("#zoo-input").val().trim();
-var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=9BlNYmwgDWvberRjgJV9QfSnyt8O6fja&limit=10";
+// var userInput = $( "#zoo-input" ).val();
+// var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=9BlNYmwgDWvberRjgJV9QfSnyt8O6fja&limit=10";
+var things = ["toast", "hamsters", "boulders", "cheese", "fish"];
 
 //functions
-console.log(userInput);
+// 
 
 console.log("two");
+
 function createDisplay(){
 
+        $("#zoo-population").empty();
+
+        // Looping through the array of movies
+        for (var i = 0; i < things.length; i++) {
+
+          // Then dynamicaly generating buttons for each thing in array
+          var a = $("<button>");
+          // Adding classes to our button
+          a.addClass("thing-button btn btn-info");
+          // Adding a data-attribute
+          a.attr("data-name", things[i]);
+          // Providing the initial button text
+          a.text(things[i]);
+          // Adding the button to the buttons-view div
+          $("#zoo-population").append(a);
+        }
 };
 
-function handleGifs(){
-    $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).done(function(response) {
-          console.log(response);
+// function handleGifs(){
 
-        });
+//         var thingItself = $(this).attr("data-name");
+//         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + thingItself + "&api_key=9BlNYmwgDWvberRjgJV9QfSnyt8O6fja&limit=10";
+
+//         $.ajax({
+//           url: queryURL,
+//           method: "GET"
+//         }).done(function(response) {
+//           console.log(response);
+
+//           // Creating a div to hold the gifs
+//           var newThingDiv = $("<div class='newGif'>");
+
+//           // Storing the rating data
+//           var rating = response.rating;
+
+//           // Creating an element to have the rating displayed
+//           var pOne = $("<p>").text("Rating: " + rating);
+
+//           // Displaying the rating
+//           newThingDiv.append(pOne);
+
+//           // Retrieving the URL for the image
+//           var imgURL = response.images.fixed_height.url;
+
+//           // Creating an element to hold the image
+//           var image = $("<img>").attr("src", imgURL);
+
+//           // Appending the image
+//           newThing.append(image);
+
+//           // Putting the entire movie above the previous movies
+//           $("#zoo-population").prepend(newThingDiv);
+//         });
+
+//       };
+
+function alterList(){
+  things.push($("#zoo-input").val());
 };
+
 
 //main processes
-$("#add-toast").click(function(){
-  handleGifs();
-  console.log(userInput);
+
+// $(document).on("click", "#add-toast", handleGifs());
+
+$("#add-things").click(function(){
+  //this keeps the divs from disappearing, and the console from freaking out.
+  event.preventDefault();
+
+  alterList();
+
+  console.log(things);
+
+  // handleGifs();
+
+
   console.log("click-button-works");
+
+  createDisplay();
+
+  $("#zoo-input").val("");
+
 });
+
+//have this function outside of the on.click for now, will move into the click when able to push new items into the array.
+// createDisplay();
 
 console.log("function inside of your function");
 
